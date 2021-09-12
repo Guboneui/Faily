4f9b693ec59dfe4195e596b9d9308e73ded7b9d7
Guboneui <starku2249@naver.com>
2021-09-13 08:53:01 +0900

    인증번호 입력 관련 메소드 추가

--- Faily/View/Login/AuthEmailViewController.swift
+++ Faily/View/Login/AuthEmailViewController.swift
@@ -19,8 +19,11 @@ class AuthEmailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         configUI()
+        firstAuthTextField.delegate = self
+        secondAuthTextField.delegate = self
+        thirdAuthTextField.delegate = self
+        fourthAuthTextField.delegate = self
     }
     
     func configUI() {
@@ -32,12 +35,66 @@ class AuthEmailViewController: UIViewController {
         
         secondAuthTextField.layer.cornerRadius = secondAuthTextField.frame.height / 2
         secondAuthTextField.keyboardType = .numberPad
+        secondAuthTextField.isEnabled = false
         
         thirdAuthTextField.layer.cornerRadius = thirdAuthTextField.frame.height / 2
         thirdAuthTextField.keyboardType = .numberPad
+        thirdAuthTextField.isEnabled = false
         
         fourthAuthTextField.layer.cornerRadius = fourthAuthTextField.frame.height / 2
         fourthAuthTextField.keyboardType = .numberPad
+        fourthAuthTextField.isEnabled = false
     }
+    
+    
+    @IBAction func firstAuthTextFieldAction(_ sender: Any) {
+        checkMaxLength(firstAuthTextField, maxLength: 1)
+    }
+    
+    @IBAction func secondAuthTextFieldAction(_ sender: Any) {
+        checkMaxLength(secondAuthTextField, maxLength: 1)
+    }
+    
+    
+    @IBAction func thirdAuthTextFieldAction(_ sender: Any) {
+        checkMaxLength(thirdAuthTextField, maxLength: 1)
+    }
+    
+    @IBAction func fourthAuthTextFieldAction(_ sender: Any) {
+        checkMaxLength(fourthAuthTextField, maxLength: 1)
+    }
+    
+}
 
+extension AuthEmailViewController: UITextFieldDelegate{
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        
+        if textField == firstAuthTextField {
+            if textField.text!.count >= 1 {
+                secondAuthTextField.isEnabled = true
+                secondAuthTextField.becomeFirstResponder()
+            }
+        } else if textField == secondAuthTextField {
+            if textField.text!.count >= 1 {
+                thirdAuthTextField.isEnabled = true
+                thirdAuthTextField.becomeFirstResponder()
+            }
+        } else if textField == thirdAuthTextField {
+            if textField.text!.count >= 1 {
+                fourthAuthTextField.isEnabled = true
+                fourthAuthTextField.becomeFirstResponder()
+            }
+        } else if textField == fourthAuthTextField {
+            if textField.text!.count >= 1 {
+                fourthAuthTextField.endEditing(true)
+            }
+        }
+ 
+    }
+    
+    func checkMaxLength(_ textField: UITextField!, maxLength: Int) {
+        if (textField.text?.count ?? 0 > maxLength) {
+            textField.deleteBackward()
+        }
+    }
 }
