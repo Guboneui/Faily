9432c99698b3166f57a091ab0c262053b5c48d4f
Guboneui <starku2249@naver.com>
2021-09-16 06:20:42 +0900

    스플래시 화면 AppDelegate에서 딜레이 로직 구현

--- Faily/DefaultFile/AppDelegate.swift
+++ Faily/DefaultFile/AppDelegate.swift
@@ -11,9 +11,8 @@ import IQKeyboardManager
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Thread.sleep(forTimeInterval: 0.5)
         UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
         UINavigationBar.appearance().shadowImage = UIImage()
         UINavigationBar.appearance().backgroundColor = .clear
