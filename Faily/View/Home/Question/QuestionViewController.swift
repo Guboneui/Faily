73b71981366732af2ffe73d972f8f4aeb6479484
Guboneui <starku2249@naver.com>
2021-09-27 03:43:21 +0900

    question VC init

--- Faily/View/Home/Question/QuestionViewController.swift
+++ Faily/View/Home/Question/QuestionViewController.swift
@@ -9,36 +9,8 @@ import UIKit
 import CollectionViewPagingLayout
 
 class QuestionViewController: UIViewController {
-    
-    
-    @IBOutlet weak var mainCollectionView: UICollectionView!
-    var collectionView: UICollectionView!
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        mainCollectionView.delegate = self
-        mainCollectionView.dataSource = self
-        mainCollectionView.register(UINib(nibName: "QuestionCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "QuestionCollectionViewCell")
-        mainCollectionView.collectionViewLayout = CollectionViewPagingLayout()
-        mainCollectionView.isPagingEnabled = true
-        
+       
     }
-    
-    
-    
-}
-
-
-extension QuestionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "QuestionCollectionViewCell", for: indexPath)
-        return cell
-    }
-    
 }
