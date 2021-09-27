caab8ac2e49d36cec2789ef284310d9300919ed5
Guboneui <starku2249@naver.com>
2021-09-28 00:23:33 +0900

    질문 날짜 로직 추가

--- Faily/View/Home/Question/MainQuestionCollectionViewCell.swift
+++ Faily/View/Home/Question/MainQuestionCollectionViewCell.swift
@@ -10,7 +10,31 @@ import CollectionViewPagingLayout
 
 class MainQuestionCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var questionBox: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerStackView: UIStackView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(goAnswerView))
+        answerStackView.addGestureRecognizer(tapGesture)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    
+    @objc func goAnswerView(_ recognizer: UITapGestureRecognizer) {
+        print("답변하러 가기")
+    }
+    
+    
+    
 }
+
+
 
 extension MainQuestionCollectionViewCell: ScaleTransformView {
     var scaleOptions: ScaleTransformViewOptions {
--- Faily/View/Home/Question/MainQuestionTableViewCell.swift
+++ Faily/View/Home/Question/MainQuestionTableViewCell.swift
@@ -10,6 +10,9 @@ import CollectionViewPagingLayout
 
 class MainQuestionTableViewCell: UITableViewCell {
 
+    
+    var dateArr = ["2021년 9월 24일", "2021년 9월 25일", "2021년 9월 26일", "2021년 9월 27일", "2021년 9월 28일"]
+    
     @IBOutlet weak var questionCollectionView: UICollectionView!
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -30,11 +33,26 @@ class MainQuestionTableViewCell: UITableViewCell {
 
 extension MainQuestionTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainQuestionCollectionViewCell", for: indexPath) as! MainQuestionCollectionViewCell
+        cell.dateLabel.text = dateArr[indexPath.item]
+   
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy년 M월 d일"
+        let current_date_string = formatter.string(from: Date())
+        print(current_date_string)
+        if cell.dateLabel.text == current_date_string {
+            cell.questionBox.image = UIImage(named: "questionBox_BookMark")
+            cell.answerStackView.isHidden = false
+        } else {
+            cell.questionBox.image = UIImage(named: "questionBox_noBookMark")
+            cell.answerStackView.isHidden = true
+        }
+    
+        
         return cell
     }
     
