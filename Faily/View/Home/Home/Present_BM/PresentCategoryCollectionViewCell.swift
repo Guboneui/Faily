e25efb089ea5803ea94494994dcd3183db71339c
Guboneui <starku2249@naver.com>
2021-10-05 12:59:02 +0900

    ios 14.5 -> 13.0 최소 버전 수정

--- Faily/View/Home/Home/Present_BM/PresentCategoryCollectionViewCell.swift
+++ Faily/View/Home/Home/Present_BM/PresentCategoryCollectionViewCell.swift
@@ -12,23 +12,23 @@ class PresentCategoryCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var baseView: UIView!
     @IBOutlet weak var bgView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
         configUI()
     }
-    
+
     func configUI() {
         baseView.layer.cornerRadius = 15
         bgView.layer.cornerRadius = 15
-//        baseView.layer.shadowColor = UIColor.FailyColor.grayscale_4.cgColor
-//        baseView.layer.shadowOffset = CGSize(width: 0, height: 4)
-//        baseView.layer.shadowOpacity = 0.33
+        baseView.layer.shadowColor = UIColor.FailyColor.grayscale_4.cgColor
+        baseView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        baseView.layer.shadowOpacity = 0.33
     }
 
 }
